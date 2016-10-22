7f31a410bdf59ec1c842a869ec79462ea34dbe38
Ibnu Yahya <ibnu@aksaramaya.com>
2016-10-22 22:01:53 +0700

    ADD: flannel restart

--- build/slave.sh
+++ build/slave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i "s|centos-minion|$HOSTNAME|g" /etc/kubernetes/kubelet
-for SERVICES in kube-proxy kubelet docker; do
+for SERVICES in kube-proxy kubelet docker flanneld; do
   systemctl restart $SERVICES
   systemctl enable $SERVICES
   systemctl status $SERVICES
