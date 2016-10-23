b30c49139c342916316837ddbe361e15292106d6
Ibnu Yahya <anak10thn@gmail.com>
2016-10-23 23:30:45 +0700

    Update master.sh

--- build/master.sh
+++ build/master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler flanneld; do
+for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler; do
   systemctl restart $SERVICES
   systemctl enable $SERVICES
   systemctl status $SERVICES
